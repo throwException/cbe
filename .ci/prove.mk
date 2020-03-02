2882e41fe9f63d47cfebea8c65c04a39c327cfee
Josef Söntgen <josef.soentgen@genode-labs.com>
2020-03-02 11:52:45 +0100

    ci: add variable to disarm warnings=errors

--- .ci/prove.mk
+++ .ci/prove.mk
@@ -1,7 +1,9 @@
 OUTDIR=$(shell pwd)/out
 
 PROVE_MODE ?= all
+WARNINGS_AS_ERRORS ?= --warnings=error
 
 prove_spark_ci:
 	make -C .ci/lsc NO_SPARK=1 DESTDIR=$(OUTDIR)/lsc install
-	gnatprove --mode=$(PROVE_MODE) -j0 --prover=z3,cvc4 --steps=1000 --memlimit=1000 --checks-as-errors --warnings=error -U -P .ci/cbe.gpr
+	gnatprove --mode=$(PROVE_MODE) -j0 --prover=z3,cvc4 --steps=1000 \
+		--memlimit=1000 --checks-as-errors $(WARNINGS_AS_ERRORS) -U -P .ci/cbe.gpr
