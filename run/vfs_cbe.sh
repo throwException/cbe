687757b6a34d58c2506fec95aeb4b9e6ef35b1dc
Josef Söntgen <josef.soentgen@genode-labs.com>
2020-05-13 18:23:05 +0200

    run/vfs_cbe: execute rekeying three times

--- run/vfs_cbe.sh
+++ run/vfs_cbe.sh
@@ -2,26 +2,6 @@
 
 echo "--- Automated CBE testing ---"
 
-test_create_snapshot() {
-	local cbe_dir="$1"
-
-	echo "Create snapshot"
-	echo true > $cbe_dir/control/create_snapshot
-}
-
-test_rw() {
-	local data_file="$1"
-	[ "$data_file" = "" ] && exit 1
-
-	# read complete current snapshot
-	echo "Read '$data_file'"
-	dd if=$data_file of=/dev/null bs=4096
-
-	# write complete current snapshot
-	echo "Write '$data_file'"
-	dd if=$data_file of=/dev/null bs=4096
-}
-
 produce_pattern() {
 	local pattern="$1"
 	local size="$2"
@@ -36,26 +16,46 @@ produce_pattern() {
 test_write_1() {
 	local data_file="$1"
 	local offset=$2
-	local pattern="$3"
+	local pattern="$offset"
 
 	local pattern_file="/tmp/pattern.$pattern"
 	produce_pattern "$pattern" "4096" > $pattern_file
-	cat $pattern_file
-	dd bs=4096 count=1 if=$pattern_file of=$data_file seek=$offset || exit 1
-	cat $pattern_file
+	dd bs=4096 count=1 if=$pattern_file of=$data_file seek=$offset 2>/dev/null || exit 1
 }
 
 test_read_compare_1() {
 	local data_file="$1"
 	local offset=$2
-	local pattern="$3"
+	local pattern="$offset"
 
 	local pattern_file="/tmp/pattern.$pattern"
 	produce_pattern "$pattern" "4096" > $pattern_file
+	rm $pattern_file.out 2>/dev/null
 
-	dd bs=4096 count=1 if=$data_file of=$pattern_file.out seek=$offset || exit 1
-	sha1sum $pattern_file
-	sha1sum $pattern_file.out
+	dd bs=4096 count=1 if=$data_file of=$pattern_file.out skip=$offset 2>/dev/null || exit 1
+	local sha1=$(sha1sum $pattern_file)
+	local sha1_sum=${sha1:0:40}
+
+	local sha1out=$(sha1sum $pattern_file.out)
+	local sha1out_sum=${sha1out:0:40}
+
+	if [ "$sha1_sum" != "$sha1out_sum" ]; then
+		echo "mismatch for block $offset:"
+		echo "  expected: $sha1_sum"
+		echo -n "  "
+		cat $pattern_file | dd bs=32 count=1 2>/dev/null; echo
+		echo "  got: $sha1out_sum"
+		echo -n "  "
+		cat $pattern_file.out | dd bs=32 count=1 2>/dev/null; echo
+		return 1
+	fi
+}
+
+test_create_snapshot() {
+	local cbe_dir="$1"
+
+	echo "Create snapshot"
+	echo true > $cbe_dir/control/create_snapshot
 }
 
 test_list_snapshots() {
@@ -81,18 +81,56 @@ test_rekey() {
 	echo "Reykeying started"
 }
 
+test_rekey_state() {
+	local cbe_dir="$1"
+	local state="$(< $cbe_dir/control/rekey)"
+
+	echo "Rekeying state: $state"
+}
+
+wait_for_rekeying() {
+	local cbe_dir="$1"
+
+	echo "Wait for rekeying to finish..."
+	while : ; do
+		local file_content="$(< $cbe_dir/control/rekey)"
+		local state="${file_content:0:4}"
+		if [ "$state" = "idle" ]; then
+			local result="${file_content:5}"
+			echo "Reykeying done: $result"
+			break;
+		fi
+		sleep 2
+	done
+}
+
 main() {
 	local cbe_dir="/dev/cbe"
 	local data_file="$cbe_dir/current/data"
 
-	test_write_1 "$data_file" "0" "1"
-	test_create_snapshot "$cbe_dir"
+	ls -l $cbe_dir
 
-	test_read_compare_1 "$data_file" "0" "1"
+	for i in $(seq 3); do
+		echo "Run $i:"
+		test_write_1 "$data_file"  "0"
+		test_write_1 "$data_file"  "8"
+		test_write_1 "$data_file" "16"
+		test_create_snapshot "$cbe_dir"
 
-	test_rekey "$cbe_dir"
+		test_read_compare_1 "$data_file" "0"
+		test_read_compare_1 "$data_file" "8"
 
-	test_read_compare_1 "$data_file" "0" "1"
+		test_rekey "$cbe_dir"
+
+		test_read_compare_1 "$data_file" "0"
+		test_rekey_state "$cbe_dir"
+		test_read_compare_1 "$data_file" "8"
+		test_rekey_state "$cbe_dir"
+		test_read_compare_1 "$data_file" "16"
+
+		wait_for_rekeying "$cbe_dir"
+		echo "Run $i done"
+	done
 
 	echo "--- Automated CBE testing finished, shell is yours ---"
 }
