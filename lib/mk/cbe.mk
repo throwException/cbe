987bb3b54275819b1295abac332d5cbc3c34fb8c
Josef Söntgen <josef.soentgen@genode-labs.com>
2020-05-20 12:45:27 +0200

    cbe trust anchor: move to cbe_common

--- lib/mk/cbe.mk
+++ lib/mk/cbe.mk
@@ -15,7 +15,6 @@ SRC_ADB += cbe-new_free_tree.adb
 SRC_ADB += cbe-meta_tree.adb
 SRC_ADB += cbe-generic_index_queue.adb
 SRC_ADB += cbe-superblock_control.adb
-SRC_ADB += cbe-trust_anchor.adb
 SRC_ADB += cbe-vbd_rekeying.adb
 
 vpath % $(REP_DIR)/src/lib/cbe
