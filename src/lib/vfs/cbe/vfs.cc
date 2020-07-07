34be608fed5fe936d9683400893f8a83779bf558
Josef Söntgen <josef.soentgen@genode-labs.com>
2020-07-07 16:35:15 +0200

    vfs/cbe: use Mutex instead of deprecated Lock

--- src/lib/vfs/cbe/vfs.cc
+++ src/lib/vfs/cbe/vfs.cc
@@ -1130,9 +1130,9 @@ class Vfs_cbe::Wrapper
 			return true;
 		}
 
-		Genode::Lock _frontend_lock { };
+		Genode::Mutex _frontend_mtx { };
 
-		Genode::Lock &frontend_lock() { return _frontend_lock; }
+		Genode::Mutex &frontend_mtx() { return _frontend_mtx; }
 };
 
 
@@ -1163,7 +1163,7 @@ class Vfs_cbe::Data_file_system : public Single_file_system
 			Read_result read(char *dst, file_size count,
 			                 file_size &out_count) override
 			{
-				Genode::Lock_guard guard { _w.frontend_lock() };
+				Genode::Mutex::Guard guard { _w.frontend_mtx() };
 
 				using State = Wrapper::Frontend_request::State;
 
@@ -1214,7 +1214,7 @@ class Vfs_cbe::Data_file_system : public Single_file_system
 			Write_result write(char const *src, file_size count,
 			                   file_size &out_count) override
 			{
-				Genode::Lock_guard guard { _w.frontend_lock() };
+				Genode::Mutex::Guard guard { _w.frontend_mtx() };
 
 				using State = Wrapper::Frontend_request::State;
 
@@ -1264,7 +1264,7 @@ class Vfs_cbe::Data_file_system : public Single_file_system
 
 			Sync_result sync() override
 			{
-				Genode::Lock_guard guard { _w.frontend_lock() };
+				Genode::Mutex::Guard guard { _w.frontend_mtx() };
 
 				using State = Wrapper::Frontend_request::State;
 
