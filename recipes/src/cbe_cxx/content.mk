482c61a7a147485dd66d32a811fdef3efc2abbd1
Josef Söntgen <josef.soentgen@genode-labs.com>
2020-08-11 11:44:02 +0200

    recipes: add src archives of C++ SAL
    Issue #65.

---
+++ recipes/src/cbe_cxx/content.mk
@@ -0,0 +1,26 @@
+MIRROR_FROM_REP_DIR := \
+	src/lib/cbe \
+	src/lib/cbe_common \
+	src/lib/cbe_cxx \
+	src/lib/cbe_cxx_common \
+	src/lib/sha256_4k \
+	lib/import/import-cbe.mk \
+	lib/import/import-cbe_common.mk \
+	lib/import/import-sha256_4k.mk \
+	lib/mk/cbe.mk \
+	lib/mk/cbe_common.mk \
+	lib/mk/cbe_cxx.mk \
+	lib/mk/cbe_cxx_common.mk \
+	lib/mk/generate_ada_main_pkg.inc \
+	lib/mk/sha256_4k.mk
+
+content: $(MIRROR_FROM_REP_DIR) src/lib/cbe_cxx/target.mk LICENSE
+
+$(MIRROR_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
+
+src/lib/cbe_cxx/target.mk: src/lib/cbe_cxx
+	echo "LIBS += cbe_cxx" > $@
+
+LICENSE:
+	cp $(REP_DIR)/LICENSE $@
